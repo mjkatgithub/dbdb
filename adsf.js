462ae13627db0157a0b2bc69ee46adb34bf40e34
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2018-02-27 14:50:26 +0100

    let's grep the port

--- adsf.js
+++ adsf.js
@@ -1,7 +1,25 @@
 const serve = require('serve');
 const path = require('path');
 
+var Port = normalizePort(process.env.PORT || '1337');
+
 const server = serve(path.join(__dirname,'dist'), {
-  port: 80,
+  port: Port,
   ignore: ['node_modules']
 })
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    //named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    //port number
+    return port;
+  }
+
+  return false;
+}
